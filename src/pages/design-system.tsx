160354c2bd90018422d814e588443ccb7b881bc8
shreyog <shreyog23@gmail.com>
2022-12-03 13:54:46 +0530

    Changelog - added design system typography

--- src/pages/design-system.tsx
+++ src/pages/design-system.tsx
@@ -7,21 +7,36 @@ import Button from "@/components/Button";
 import Input from "@/components/Input";
 
 const Main = styled.main({
-  height: "100vh",
+  minHeight: "100vh",
+  height: "100%",
+  maxWidth: "700px",
+  margin: "auto",
   backgroundColor: "var(--clr-background-main)",
 });
 
 const Section = styled.section({
-  display: "flex",
-  flexDirection: "column",
-  gap: "12px",
-  width: "600px",
-  margin: "auto",
+  paddingBlockEnd: "1.5rem",
 });
 
-const TextH3 = styled.h1({
-  textAlign: "center",
-  color: "var(--clr-text-main)",
+const ComponentSectionWrapper = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  gap: "16px",
+});
+
+const Hr = styled.hr({ marginBottom: "1.5rem", marginTop: "0.7rem" });
+
+const PaletteContainer = styled.div({
+  display: "flex",
+});
+
+const Color = styled.div({
+  display: "inline-flex",
+  border: "2px solid #000000",
+  borderRadius: "50%",
+  height: "70px",
+  width: "70px",
+  marginRight: "1rem",
 });
 
 const DesignSystemPage: React.FC<PageProps> = () => {
@@ -29,10 +44,43 @@ const DesignSystemPage: React.FC<PageProps> = () => {
     <HomeLayout>
       <Main>
         <Section>
-          <TextH3>😎 Components 🚀</TextH3>
-          <Button variant="outlined">outlined</Button>
-          <Button variant="filled">filled</Button>
-          <Input placeholder={"Input"} />
+          <p className="fv-subtitle fw-medium text-accent">Palette</p>
+          <Hr />
+          <PaletteContainer>
+            <Color
+              style={{ backgroundColor: "var(--clr-background-main)" }}
+            ></Color>
+            <Color
+              style={{ backgroundColor: "var(--clr-background-alt)" }}
+            ></Color>
+            <Color style={{ backgroundColor: "var(--clr-text-main)" }}></Color>
+            <Color style={{ backgroundColor: "var(--clr-text-alt)" }}></Color>
+            <Color style={{ backgroundColor: "var(--clr-accent)" }}></Color>
+          </PaletteContainer>
+        </Section>
+        <Section>
+          <p className="fv-subtitle fw-medium text-accent">Typography</p>
+          <Hr />
+          <h1 className="fv-h1">The quick brown fox jumps over the lazy dog</h1>
+          <h2 className="fv-h2">The quick brown fox jumps over the lazy dog</h2>
+          <h3 className="fv-h3">The quick brown fox jumps over the lazy dog</h3>
+          <h4 className="fv-h4">The quick brown fox jumps over the lazy dog</h4>
+          <h5 className="fv-h5">The quick brown fox jumps over the lazy dog</h5>
+          <h6 className="fv-h6">The quick brown fox jumps over the lazy dog</h6>
+          <h6 className="fv-subtitle">
+            The quick brown fox jumps over the lazy dog
+          </h6>
+          <p className="fv-p">The quick brown fox jumps over the lazy dog</p>
+        </Section>
+
+        <Section>
+          <p className="fv-subtitle fw-medium text-accent">Components</p>
+          <Hr />
+          <ComponentSectionWrapper>
+            <Button variant="outlined">outlined</Button>
+            <Button variant="filled">filled</Button>
+            <Input placeholder={"Input"} />
+          </ComponentSectionWrapper>
         </Section>
       </Main>
     </HomeLayout>
@@ -41,4 +89,4 @@ const DesignSystemPage: React.FC<PageProps> = () => {
 
 export default DesignSystemPage;
 
-export const DesignSystem: HeadFC = () => <title>Design System Page</title>;
+export const Head: HeadFC = () => <title>Design System</title>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+import * as React from "react"
+
 import type { HeadFC, PageProps } from "gatsby";
 
 import HomeLayout from "@/layouts/HomeLayout";
